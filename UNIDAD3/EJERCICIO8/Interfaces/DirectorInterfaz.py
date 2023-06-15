619f2f10b510b8d362fe9884616aa647b27cacd7
agusals <agusals2001@gmail.com>
2023-06-14 23:56:43 -0300

    ejers3

---
+++ UNIDAD3/EJERCICIO8/Interfaces/DirectorInterfaz.py
@@ -0,0 +1,19 @@
+from zope.interface import Interface
+
+class IDirector (Interface):
+
+   def modificarBasico(dni, nuevoBasico):
+
+       pass
+
+   def modificarPorcentajeporcargo(dni, nuevoPorcentaje):
+
+     pass
+
+   def modificarPorcentajeporcategoría(dni, nuevoPorcentaje):
+
+       pass
+
+def modificarImporteExtra(dni, nuevoImporteExtra):
+
+       pass
